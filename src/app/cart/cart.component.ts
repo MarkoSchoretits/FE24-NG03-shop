dfa2719d3fbd3ea6c13a99d1feb082bfa75951c0
MarkoSchoretits <marko@schoretits.com>
2023-02-23 13:50:54 +0100

    C add cart display

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { IProducts } from '../IProducts';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+  items: IProducts[] = [];
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.items = this.cartService.getItems();
+  }
+
+}
