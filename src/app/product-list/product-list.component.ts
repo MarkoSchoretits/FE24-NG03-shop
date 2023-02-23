2481da30428ed1f1d664bffd394cc49d0e29fd54
MarkoSchoretits <marko@schoretits.com>
2023-02-23 11:30:37 +0100

    C add product list loop

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
+import { IProducts } from '../IProducts';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products:IProducts[] = products;
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
 }
